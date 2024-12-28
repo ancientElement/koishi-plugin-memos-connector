b5245f93fa4603b7078236fa19a4eb413e100134
ancientElement <642211794@qq.com>
2024-12-29 01:28:25 +0800

    使用中间件解决emm重复输入导致bug的问题

--- src/index.ts
+++ src/index.ts
@@ -36,6 +36,9 @@ const addMemo = async (ctx: Context,config: Config,content: any,visibility: stri
 }
 
 let startContinuousInput = false
+let memoContextFianl = ""
+let memoCreatRes : HTTP.Response<any>
+let memoVisibility = VISIBILITY.PRIVATE
 
 export function apply(ctx: Context, config: Config) {
     // write your plugin here
@@ -43,33 +46,42 @@ export function apply(ctx: Context, config: Config) {
     .option("public", "-p 创建公开memo",{fallback:false})
     .option("protected", "-o 创建受保护memo",{fallback:false})
     .option("private", "-i 创建私有memo",{fallback:false})
-    .action(async ({session,options},message)=>{
-        let res
-        const visibility = options.public ? VISIBILITY.PUBLIC : options.protected ? VISIBILITY.PROTECTED : VISIBILITY.PRIVATE
+    .action(async ({options},message)=>{
+        memoVisibility = options.public ? VISIBILITY.PUBLIC : options.protected ? VISIBILITY.PROTECTED : VISIBILITY.PRIVATE
         if (message){
-            res = await addMemo(ctx,config,message,visibility)
+            memoCreatRes = await addMemo(ctx,config,message,memoVisibility)
         } else {
-            await session.send("请输入要创建的memo内容\n使用emm作为最后一条消息")
             startContinuousInput = true
-            let context = await session.prompt(60000)
-            let contextFianl = context
-            while (context && context != "emm"){
-                context = await session.prompt(60000)
-                contextFianl = `${contextFianl}\n${context}`
-            }
-            if (!contextFianl) return "没有输入内容"
-            res = await addMemo(ctx,config,contextFianl,visibility)
+            return "请输入要创建的memo内容\n使用emm作为最后一条消息"
         }
-        const name = res.data.name
-        const creator = res.data.creator
-        const visibilityString = res.data.visibility
-        const createTime = res.data.createTime
+        const name = memoCreatRes.data.name
+        const creator = memoCreatRes.data.creator
+        const visibilityString = memoCreatRes.data.visibility
+        const createTime = memoCreatRes.data.createTime
         return `memo创建成功！\n名称：${name}\n创建者：${creator}\n可见性：${visibilityString}\n创建时间：${createTime}`
     })
 
+    ctx.middleware(async (session, next) => {
+        if (startContinuousInput && session.content !== "emm") {
+            memoContextFianl = `${memoContextFianl}\n${session.content}`
+        } else {
+            return next()
+        }
+    },true)
+
     ctx.command("emm","结束memos")
-    .action(async ()=>{
+    .option("public", "-p 创建公开memo",{fallback:false})
+    .option("protected", "-o 创建受保护memo",{fallback:false})
+    .option("private", "-i 创建私有memo",{fallback:false})
+    .action(async ({options})=>{
         if (!startContinuousInput) return "没有正在输入的memo"
         startContinuousInput = false
+        memoVisibility = options.public ? VISIBILITY.PUBLIC : options.protected ? VISIBILITY.PROTECTED : VISIBILITY.PRIVATE
+        memoCreatRes = await addMemo(ctx,config,memoContextFianl,memoVisibility)
+        const name = memoCreatRes.data.name
+        const creator = memoCreatRes.data.creator
+        const visibilityString = memoCreatRes.data.visibility
+        const createTime = memoCreatRes.data.createTime
+        return `memo创建成功！\n名称：${name}\n创建者：${creator}\n可见性：${visibilityString}\n创建时间：${createTime}`
     })
 }
